2bace58f57cea5fdda6fb9859fb12dd2d6fbbfde
Kleidi Bujari <mail@4kb.net>
2025-07-15 21:19:33 -0400

    meta: init recursive attrset building based on directory structure

---
+++ default.nix
@@ -0,0 +1,42 @@
+{
+  localSystem ? builtins.currentSystem,
+  crossSystem ? localSystem,
+  ...
+}:
+
+let
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/7c4f34d1d13a4776aa1c96183ddfd6da2fdd601d.tar.gz";
+    sha256 = "0ys4mvl9f7slaix1av676ya1j2kscyc6b79fwl3mdgvrzahbkkr0";
+  };
+
+  pkgs = import nixpkgs {
+    inherit localSystem crossSystem;
+    config = { };
+  };
+
+  inherit (pkgs) lib;
+  fs = lib.fileset;
+  inherit (lib)
+    callPackageWith
+    filesystem
+    ;
+
+  callPackage = callPackageWith (pkgs // { inherit world; });
+
+  sourceFiles = fs.difference ./. (
+    fs.unions [
+      (fs.maybeMissing ./result)
+      ./default.nix
+    ]
+  );
+
+  world = filesystem.packagesFromDirectoryRecursive {
+    inherit callPackage;
+    directory = fs.toSource {
+      root = ./.;
+      fileset = sourceFiles;
+    };
+  };
+in
+world
